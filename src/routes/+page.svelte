aeb0a3ba8b6c05d446cf8b32ac084ec9602aece6
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-12-03 21:02:18 +0100

    ✨ feat(plugin): assert bilateral communication works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,4 +26,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button>hello world</button>
+<button onclick={sendPing}>hello world</button>
