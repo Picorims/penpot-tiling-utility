14c61d62576cd04f4996f313f109eae9e60f5437
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-12-15 22:17:52 +0100

    🐳 chore(plugin): deploy

--- docs/_app/immutable/entry/start.B5NJZBjU.js
+++
@@ -1,2 +0,0 @@
-import{a as t}from"../chunks/entry.DhUAiJee.js";export{t as start};
-//# sourceMappingURL=start.B5NJZBjU.js.map
---
+++ docs/_app/immutable/entry/start.BpUqF07q.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.CH-tfclc.js";export{t as start};
+//# sourceMappingURL=start.BpUqF07q.js.map
